a8a8866e70b480de947729db3e7e2a238a19a1b7
Stay2003 <3107479638@qq.com>
2025-01-01 13:46:28 +0800

    后端基础开发-CRUD

---
+++ src/main/java/com/yupi/mianshikun/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.mianshikun.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.mianshikun.model.entity.QuestionBankQuestion;
+
+/**
+* @author 31074
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @createDate 2025-01-01 13:15:58
+* @Entity generator.domain.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/mianshikun/model/entity/QuestionBankQuestion.java
@@ -0,0 +1,50 @@
+package com.yupi.mianshikun.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 题库题目
+ * @TableName question_bank_question
+ */
+@TableName(value ="question_bank_question")
+@Data
+public class QuestionBankQuestion implements Serializable {
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 题库 id
+     */
+    private Long questionBankId;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @TableLogic
+    private Date updateTime;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
