fe82cbcd4814d00f632921ded12e98585c7a5e52
Stay2003 <3107479638@qq.com>
2025-01-02 14:41:19 +0800

    基础开发完成，后面就是自己写业务了

--- src/main/java/com/yupi/mianshikun/controller/QuestionController.java
+++ src/main/java/com/yupi/mianshikun/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package com.yupi.mianshikun.controller;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yupi.mianshikun.annotation.AuthCheck;
 import com.yupi.mianshikun.common.BaseResponse;
@@ -24,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 题目接口
@@ -218,6 +221,10 @@ public class QuestionController {
         // todo 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionEditRequest, question);
+        List<String> tags = questionEditRequest.getTags();
+        if (tags != null) {
+            question.setTags(JSONUtil.toJsonStr(tags));
+        }
         // 数据校验
         questionService.validQuestion(question, false);
         User loginUser = userService.getLoginUser(request);
